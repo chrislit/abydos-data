aef46c7b62aa320d3ce449eeb879fe7200437ddd
Chris Little <chrisclittle@gmail.com>
2019-04-07 14:49:01 -0700

    added packages to base url

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,1 @@
-tools/build_pkg_index.py . https://raw.githubusercontent.com/chrislit/abydos-data/master index.xml
+tools/build_pkg_index.py . https://raw.githubusercontent.com/chrislit/abydos-data/master/packages index.xml
